c8ce9574b1cc058592331d6409ce83720dfbbf1e
PreranaMalge <preranamalge123@gmail.com>
2022-05-20 16:34:29 +0530

    toggle

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
   
-  const [showbtn,setShowBtn] = useState(false)
-  const [showheading,setShowHeading] = useState(false)
-
-  
+  const [texttoggle,setTextToggle] = useState(false)
 
   return (
     <div id="main">
-     {showheading && <h1 id='marco-polo' onClick={()=>setShowBtn(!showbtn)}>Marco</h1>} 
-    {showbtn && <button id='marco-polo-toggler' onClick={()=>setShowHeading(!showheading)}>Polo</button> }  
+
+     <h1 id='marco-polo'>Marco</h1> 
+     <button id='marco-polo-toggler' onClick={()=>setTextToggle(!texttoggle)}>Polo</button> 
+
 
     </div>
   )
