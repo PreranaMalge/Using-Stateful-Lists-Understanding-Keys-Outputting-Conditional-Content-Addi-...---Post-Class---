3bb449d75925493723efc1e41a359fe4b999afde
PreranaMalge <preranamalge123@gmail.com>
2022-05-20 16:22:49 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
   
+  const [showbtn,setShowBtn] = useState(false)
+  const [showheading,setShowHeading] = useState(false)
+
+  
+
   return (
     <div id="main">
+     {showheading && <h1 id='marco-polo' onClick={()=>setShowBtn(!showbtn)}>Marco</h1>} 
+    {showbtn && <button id='marco-polo-toggler' onClick={()=>setShowHeading(!showheading)}>Polo</button> }  
 
     </div>
   )
