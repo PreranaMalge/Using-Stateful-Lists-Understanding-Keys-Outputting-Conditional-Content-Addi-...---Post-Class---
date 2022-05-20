6cd3db09b253cbec999889950d1e867e8c16fcbf
PreranaMalge <preranamalge123@gmail.com>
2022-05-20 16:58:55 +0530

    toggle1

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
   
-  const [texttoggle,setTextToggle] = useState(false)
+  const [texttoggle,setTextToggle] = useState('Marco')
+
+  const handleToggle =()=>{
+    
+    const val = texttoggle ;
+    
+    val === 'Marco' ? setTextToggle('Polo') : setTextToggle('Marco')
+    
+  }
+
+  
 
   return (
     <div id="main">
 
-     <h1 id='marco-polo'>Marco</h1> 
-     <button id='marco-polo-toggler' onClick={()=>setTextToggle(!texttoggle)}>Polo</button> 
+     <h1 id='marco-polo'>{texttoggle}</h1> 
+     <button id='marco-polo-toggler' onClick={handleToggle} >click me</button> 
 
 
     </div>
